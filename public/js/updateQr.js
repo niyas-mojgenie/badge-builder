501f62bf9c3c4bbf252216e282ca4b35db8db1a0
niyas-mojgenie <niyas@mojgenie.com>
2024-06-03 09:10:58 +0530

    qr issue solved

---
+++ public/js/updateQr.js
@@ -0,0 +1,26 @@
+function updateQr(e) {
+    const qrToggle = document.getElementById("toggle-qr");
+    const qr = document.getElementById("qr-image");
+    const widthInput = document.getElementById("floating_qr_width");
+    const heightInput = document.getElementById("floating_qr_height");
+    const width = parseFloat(widthInput.value); // Convert input value to number
+    const height = parseFloat(heightInput.value); // Convert input value to number
+
+    e.preventDefault();
+    closeModal("qr-modal");
+
+    if (qrToggle.checked) {
+        console.log("checked");
+        qr.style.display = "block";
+        // Set the width and height of the canvas div
+        if (!isNaN(width) && !isNaN(height)) {
+            qr.style.width = `${width}px`;
+            qr.style.height = `${height}px`;
+        } else {
+            // Handle invalid input (if needed)
+            console.error("Invalid width or height entered");
+        }
+    } else {
+        qr.style.display = "none";
+    }
+}
