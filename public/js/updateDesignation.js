274808a11b2a64d2c017b32554016e6c4a38473b
niyas-mojgenie <niyas@mojgenie.com>
2024-06-06 10:08:47 +0530

    designation completed

---
+++ public/js/updateDesignation.js
@@ -0,0 +1,46 @@
+function updateDesignation(e) {
+    const deligateNameToggle = document.getElementById("toggle-designation");
+    const deligateName = document.getElementById("designation");
+    const FormfontWeight = document.getElementById(
+        "floating_designation_font_weight"
+    );
+    const Formfontsize = document.getElementById(
+        "floating_designation_font_size"
+    );
+
+    const fontSize = window.getComputedStyle(deligateName).fontSize;
+    const fontWeight = window.getComputedStyle(deligateName).fontWeight;
+
+    const fontFamilySelect = document.getElementById("font-family-designation");
+    const fontFamily = fontFamilySelect.value;
+    const textAlignSelect = document.getElementById("text-align-designation");
+    const textAlign = textAlignSelect.value;
+
+    const colorInput = document.getElementById("favcolor-designation");
+    const textMaxWidth = document.getElementById(
+        "floating_designation_max_width"
+    );
+
+    e.preventDefault();
+    closeModal("designation-modal");
+
+    colorInput.addEventListener("input", function (event) {
+        const selectedColor = event.target.value;
+        console.log("Selected color:", selectedColor);
+        colorInput.value = selectedColor;
+        // You can perform any additional actions here if needed
+    });
+
+    if (deligateNameToggle.checked) {
+        console.log("checked");
+        deligateName.style.display = "block";
+        deligateName.style.fontSize = Formfontsize.value + "px";
+        deligateName.style.fontWeight = FormfontWeight.value;
+        deligateName.style.color = colorInput.value;
+        deligateName.style.maxWidth = `${textMaxWidth.value}px`;
+        deligateName.style.textAlign = textAlign;
+        deligateName.style.fontFamily = fontFamily;
+    } else {
+        deligateName.style.display = "none";
+    }
+}
